0796596c07e1297521f05935479deb9b3c856252
Denis Talalaev <71027278+DenisTalalaev@users.noreply.github.com>
2021-11-24 22:56:06 +0300

    Update ControllerTest.java

--- src/test/java/com/example/mendeleevtable/ControllerTest.java
+++ src/test/java/com/example/mendeleevtable/ControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class ControllerTest {
+public class ControllerTest {
 
     final String ELEMENT = "ВоДОрОд";
 
