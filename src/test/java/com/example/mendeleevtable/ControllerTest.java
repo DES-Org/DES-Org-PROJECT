05574ac940d8d20c843b3837c7b6218ea87703df
Denis Talalaev <71027278+DenisTalalaev@users.noreply.github.com>
2021-11-24 23:29:41 +0300

    Update ControllerTest.java

--- src/test/java/com/example/mendeleevtable/ControllerTest.java
+++ src/test/java/com/example/mendeleevtable/ControllerTest.java
@@ -3,11 +3,9 @@ package com.example.mendeleevtable;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ControllerTest {
-
-    final String ELEMENT = "ВоДОрОд";
 
     @Test
     public void makeStringLower_Test() {
@@ -17,18 +15,35 @@ public class ControllerTest {
     }
 
     @Test
-    public void shuffleArray_Test() {
-        final int numberOfElements = 1;
-        int[] arr = new int[]{1};
-        int[] shuffledArr = Controller.shuffleArray(numberOfElements);
-        Assertions.assertArrayEquals(arr, shuffledArr);
-    }
-
-    @Test
     public void deleteAllSpaces_Test() {
         String str = "пр  и вет     ";
         String expected = "привет";
         String actual = Controller.deleteAllSpaces(str);
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void takeRightAnswer_Test() {
+        int num = 66;
+        String expected = "диспрозий";
+        String actual = expected + Controller.deleteAllSpaces(Controller.makeStringLower(Controller.takeRightAnswer(num)));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void takeLogo_Test() {
+        int num = 101;
+        String expected = "Md";
+        String actual = expected + Controller.takeLogo(num);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shuffleArray_Test() {
+        int arr[] = new int[118];
+        for (int i = 0; i < 118; i++)
+            arr[i] = i + 1;
+        int[] actual = Controller.shuffleArray(118);
+        assertFalse(arr.equals(actual));
     }
 }
