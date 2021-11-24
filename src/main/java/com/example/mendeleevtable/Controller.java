81bf28617056a6ee95efe987c952afa7c2885556
Denis Talalaev <71027278+DenisTalalaev@users.noreply.github.com>
2021-11-25 02:36:12 +0300

    Update Controller.java

--- src/main/java/com/example/mendeleevtable/Controller.java
+++ src/main/java/com/example/mendeleevtable/Controller.java
@@ -20,10 +20,6 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 
-import static java.lang.System.err;
-import static java.lang.System.in;
-
-
 public class Controller extends HelloApplication{
 
 
