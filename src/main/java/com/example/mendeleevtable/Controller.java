fcbb8b5d0b83de9185c6fb28e75c905f2c6a2891
l1arvakar <83829251+l1arvakar@users.noreply.github.com>
2021-11-25 04:10:35 +0300

    Update Controller.java

--- src/main/java/com/example/mendeleevtable/Controller.java
+++ src/main/java/com/example/mendeleevtable/Controller.java
@@ -17,7 +17,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.security.SecureRandom;
 import java.util.Objects;
 import java.util.Random;
 
@@ -211,7 +210,7 @@ public class Controller extends HelloApplication{
     private static int gameHealth = 3;
     private static int[] gameArr = new int[118];
 
-    private static final SecureRandom rnd = new SecureRandom();
+    private static final Random rnd = new Random();
 
     public static String takeRightAnswer(int number){
         String name = "";
@@ -416,9 +415,15 @@ public class Controller extends HelloApplication{
     public static int choosingGameDifficulty(String choice){
         int numberOfElements;
         switch (choice) {
-            case ("Easy") -> numberOfElements = 30;
-            case ("Normal") -> numberOfElements = 60;
-            case ("Hard") -> numberOfElements = 118;
+            case ("Easy") -> {
+                numberOfElements = 30;
+            }
+            case ("Normal") -> {
+                numberOfElements = 60;
+            }
+            case ("Hard") -> {
+                numberOfElements = 118;
+            }
             default -> numberOfElements = 100;
         }
         return numberOfElements;
@@ -540,14 +545,17 @@ public class Controller extends HelloApplication{
 
     public static int[][] cutArr(int counter, int size, int[][] answer){
         int[][] arr = new int[counter][size];
-        System.arraycopy(answer, 0, arr, 0, counter);
+        for (int i = 0; i < counter; i++)
+            arr[i] = answer[i];
         return arr;
     }
 
     public static int[][] sortWSteps(int[] arr) {
         int[][] answer = new int[arr.length][arr.length];
         int counter = 1;
-        System.arraycopy(arr, 0, answer[0], 0, arr.length);
+        for (int k = 0; k < arr.length; k++){
+            answer[0][k] = arr[k];
+        }
         for (int i = 1; i < arr.length; i++){
             int current = arr[i];
             int j = i;
@@ -557,7 +565,9 @@ public class Controller extends HelloApplication{
             }
             arr[j] = current;
             if (j < i) {
-                System.arraycopy(arr, 0, answer[counter], 0, arr.length);
+                for (int k = 0; k < arr.length; k++){
+                    answer[counter][k] = arr[k];
+                }
                 counter++;
             }
         }
@@ -618,8 +628,10 @@ public class Controller extends HelloApplication{
         int[] rightArray;
         rightArray = new int [lengthRight];
 
-        System.arraycopy(array, left, leftArray, 0, lengthLeft);
-        System.arraycopy(array, mid, rightArray, 0, lengthRight);
+        for (int i = 0; i < lengthLeft; i++)
+            leftArray[i] = array[left+i];
+        for (int i = 0; i < lengthRight; i++)
+            rightArray[i] = array[mid+i];
 
         int leftIndex = 0;
         int rightIndex = 0;
